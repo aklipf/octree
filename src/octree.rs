dea02c5830dc99b93a510a0d95ea180c21650b60
Astrid Klipfel <astridklipfel@gmail.com>
2024-06-11 13:41:43 +0200

    octree with default trait and wip add point

--- src/octree.rs
+++ src/octree.rs
@@ -1,37 +1,147 @@
 use glam::Vec3;
 use num_traits::One;
 
-use crate::types::{Index, Vector3D};
+use crate::types::Index;
 
-struct Octree<V: Vector3D = Vec3, I: Index = u32, const B: usize = 32> {
-    pub(crate) stems: Vec<StemNode<I>>,
-    pub(crate) leafs: Vec<LeafNode<V, I, B>>,
-    pub(crate) center: V,
-    pub(crate) size: <V as Vector3D>::Scalar,
-}
-
-struct StemNode<I: Index> {
-    pub(crate) parent: I,
-    pub(crate) childs: [I; 8],
-}
-
-struct LeafNode<V: Vector3D, I: Index, const B: usize> {
-    pub(crate) points: [V; B],
-    pub(crate) parent: I,
+pub(crate) struct Node<I: Index> {
+    pub(crate) data: I,
     pub(crate) size: I,
 }
 
-impl<V: Vector3D, I: Index, const B: usize> Default for Octree<V, I, B>
-where
-    V::Scalar: One,
-{
+pub(crate) struct PointsBlock<const B: usize> {
+    pub(crate) points: [Vec3; B],
+}
+
+pub(crate) struct NodeBlock<I: Index> {
+    pub(crate) parent: I,
+    pub(crate) nodes: [Node<I>; 8],
+}
+
+pub struct Octree<I: Index = usize, const B: usize = 5> {
+    pub(crate) root: Node<I>,
+    pub(crate) nodes: Vec<NodeBlock<I>>,
+    pub(crate) points: Vec<PointsBlock<B>>,
+    pub(crate) center: Vec3,
+    pub(crate) size: f32,
+}
+
+impl<I: Index> Default for Node<I> {
     fn default() -> Self {
-        Self {
-            stems: Default::default(),
-            leafs: Default::default(),
-            center: V::ZERO,
-            size: V::Scalar::one(),
+        Node::<I> {
+            data: I::empty(),
+            size: I::zero(),
         }
     }
 }
 
+impl<const B: usize> Default for PointsBlock<B> {
+    fn default() -> Self {
+        PointsBlock::<B> {
+            points: [Vec3::ZERO; B],
+        }
+    }
+}
+
+impl<I: Index, const B: usize> Default for Octree<I, B> {
+    fn default() -> Self {
+        Self {
+            root: Node {
+                data: I::idx(0, 0),
+                size: I::zero(),
+            },
+            nodes: vec![NodeBlock {
+                parent: I::empty(),
+                nodes: Default::default(),
+            }],
+            points: Default::default(),
+            center: Vec3::ZERO,
+            size: 2.0,
+        }
+    }
+}
+
+impl<I: Index, const B: usize> Octree<I, B> {
+    pub fn add(&mut self, point: Vec3) {
+        let (node_idx, center, size) = self.locate_node(point);
+        self.insert(node_idx, point, center, size);
+    }
+
+    fn locate_node(&self, point: Vec3) -> ((usize, usize), Vec3, f32) {
+        let mut current: &Node<I> = &self.root;
+        let mut child_idx: usize = 0;
+        let mut block_idx: usize = 0;
+        let mut center: Vec3 = self.center;
+        let mut size: f32 = self.size;
+
+        while !current.data.is_leaf() {
+            child_idx = Self::branch(point, &mut center, &mut size);
+            block_idx = current.data.node_block();
+            current = self.node((block_idx, child_idx));
+        }
+
+        ((block_idx, child_idx), center, size)
+    }
+
+    fn insert(&mut self, node_idx: (usize, usize), point: Vec3, center: Vec3, size: f32) {
+        let node: &mut Node<I> = self.mut_node(node_idx);
+
+        if node.data.is_empty() {
+            node.size = I::one();
+
+            let mut leaf_points: PointsBlock<B> = Default::default();
+            leaf_points.points[0] = point;
+            self.points.push(leaf_points);
+        } else if node.size.into() < B {
+            let new_size = node.size + I::one();
+            node.size = new_size;
+
+            let points_idx = node.data.points_block();
+            self.points[points_idx].points[new_size.into()] = point;
+        }
+    }
+
+    #[inline]
+    fn node(&self, idx: (usize, usize)) -> &Node<I> {
+        &self.nodes[idx.0].nodes[idx.1]
+    }
+
+    #[inline]
+    fn mut_node(&mut self, idx: (usize, usize)) -> &mut Node<I> {
+        &mut self.nodes[idx.0].nodes[idx.1]
+    }
+
+    #[inline]
+    fn branch(point: Vec3, center: &mut Vec3, size: &mut f32) -> usize {
+        let diff = point - *center;
+        *center += (*size * 0.25) * diff.signum();
+        *size *= 0.5;
+
+        diff.is_negative_bitmask() as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use glam::Vec3;
+
+    use crate::types::Index;
+
+    use super::Octree;
+
+    #[test]
+    fn octree_default() {
+        let tree: Octree = Octree::default();
+
+        assert_eq!(tree.size, 2.0);
+        assert_eq!(tree.center, Vec3::ZERO);
+        assert_eq!(tree.points.len(), 0);
+        assert_eq!(tree.nodes.len(), 1);
+        assert_eq!(tree.nodes[0].parent, usize::empty());
+        for i in 0..8 {
+            assert_eq!(tree.nodes[0].nodes[i].size, 0);
+            assert_eq!(tree.nodes[0].nodes[i].data, usize::empty());
+        }
+        assert_eq!(tree.root.size, 0);
+        assert_eq!(tree.root.data, usize::idx(0, 0));
+    }
+}
--- src/types.rs
+++ src/types.rs
@@ -1,44 +1,75 @@
-use glam::{DVec3, Vec3};
-use num_traits::{PrimInt, Unsigned};
+use std::fmt::Debug;
 
-pub trait Vector3D {
+use glam::{DVec3, Vec3};
+use num_traits::{Float, PrimInt, Unsigned};
+
+pub trait Vector3D
+where
+    Self::Scalar: Float,
+{
     type Scalar;
     const ZERO: Self;
 }
 
 impl Vector3D for Vec3 {
     type Scalar = f32;
-    const ZERO: Self = Self::ZERO;
+    const ZERO: Self = Vec3::ZERO;
 }
 impl Vector3D for DVec3 {
     type Scalar = f64;
-    const ZERO: Self = Self::ZERO;
+    const ZERO: Self = DVec3::ZERO;
 }
 
-pub trait Index: PrimInt + Unsigned {
-    type IDX;
-
+pub trait Index: PrimInt + Unsigned + Into<usize> {
+    fn mask() -> Self;
+    fn empty() -> Self;
+    fn idx(block: usize, child: usize) -> Self;
+    fn child(&self) -> usize;
+    fn node_block(&self) -> usize;
+    fn points_block(&self) -> usize;
     fn is_leaf(&self) -> bool;
-    fn mask(&self) -> Self::IDX;
-    fn index(&self) -> Self::IDX;
+    fn is_empty(&self) -> bool;
 }
 
-impl<T: PrimInt + Unsigned> Index for T {
-    type IDX = T;
+impl Index for usize {
+    #[inline]
+    fn mask() -> Self {
+        !(Self::max_value() << 3)
+    }
 
     #[inline]
-    fn mask(&self) -> Self::IDX {
-        T::max_value() >> 1
+    fn empty() -> Self {
+        Self::max_value()
+    }
+
+    #[inline]
+    fn idx(block: usize, child: usize) -> Self {
+        (block << 3) | child
+    }
+
+    #[inline]
+    fn child(&self) -> usize {
+        *self & Self::mask()
+    }
+
+    #[inline]
+    fn node_block(&self) -> usize {
+        *self >> 3
+    }
+
+    #[inline]
+    fn points_block(&self) -> usize {
+        *self & (Self::max_value() >> 1)
     }
 
     #[inline]
     fn is_leaf(&self) -> bool {
-        *self > self.mask()
+        (*self & (!(Self::max_value() >> 1))) != 0usize
     }
 
     #[inline]
-    fn index(&self) -> Self::IDX {
-        *self & self.mask()
+    fn is_empty(&self) -> bool {
+        *self == Self::empty()
     }
 }
 
@@ -47,56 +78,43 @@ mod tests {
     use super::Index;
 
     #[test]
-    fn check_is_leaf() {
-        assert_eq!(0u8.is_leaf(), false);
-        assert_eq!(127u8.is_leaf(), false);
-        assert_eq!(128u8.is_leaf(), true);
-        assert_eq!(255u8.is_leaf(), true);
-
-        assert_eq!(0u16.is_leaf(), false);
-        assert_eq!(32767u16.is_leaf(), false);
-        assert_eq!(32768u16.is_leaf(), true);
-        assert_eq!(65535u16.is_leaf(), true);
-
-        assert_eq!(0u32.is_leaf(), false);
-        assert_eq!(2147483647u32.is_leaf(), false);
-        assert_eq!(2147483648u32.is_leaf(), true);
-        assert_eq!(4294967295u32.is_leaf(), true);
-
-        assert_eq!(0u64.is_leaf(), false);
-        assert_eq!(9223372036854775807u64.is_leaf(), false);
-        assert_eq!(9223372036854775808u64.is_leaf(), true);
-        assert_eq!(18446744073709551615u64.is_leaf(), true);
+    fn test_child() {
+        assert_eq!(0usize.child(), 0usize);
+        assert_eq!(4usize.child(), 4usize);
+        assert_eq!(7usize.child(), 7usize);
+        assert_eq!(16usize.child(), 0usize);
+        assert_eq!(2980usize.child(), 4usize);
+        assert_eq!(6031usize.child(), 7usize);
     }
 
     #[test]
-    fn check_mask() {
-        assert_eq!(0u8.mask(), (1u8 << 7) - 1);
-        assert_eq!(0u16.mask(), (1u16 << 15) - 1);
-        assert_eq!(0u32.mask(), (1u32 << 31) - 1);
-        assert_eq!(0u64.mask(), (1u64 << 63) - 1);
+    fn test_mask() {
+        assert_eq!(usize::mask(), 7);
     }
 
     #[test]
-    fn check_index() {
-        assert_eq!(0u8.index(), 0u8);
-        assert_eq!(127u8.index(), 127u8);
-        assert_eq!(128u8.index(), 0u8);
-        assert_eq!(255u8.index(), 127u8);
+    fn test_block() {
+        assert_eq!(0.node_block(), 0);
+        assert_eq!(4.node_block(), 0);
+        assert_eq!(7.node_block(), 0);
+        assert_eq!(16.node_block(), 2);
+        assert_eq!(2980.node_block(), 372);
+        assert_eq!(6031.node_block(), 753);
+    }
 
-        assert_eq!(0u16.index(), 0u16);
-        assert_eq!(32767u16.index(), 32767u16);
-        assert_eq!(32768u16.index(), 0u16);
-        assert_eq!(65535u16.index(), 32767u16);
+    #[test]
+    fn test_is_empty() {
+        assert_eq!(0.is_empty(), false);
+        assert_eq!((usize::max_value() >> 1).is_empty(), false);
+        assert_eq!(((usize::max_value() >> 1) + 1).is_empty(), false);
+        assert_eq!(usize::max_value().is_empty(), true);
+    }
 
-        assert_eq!(0u32.index(), 0u32);
-        assert_eq!(2147483647u32.index(), 2147483647u32);
-        assert_eq!(2147483648u32.index(), 0u32);
-        assert_eq!(4294967295u32.index(), 2147483647u32);
-
-        assert_eq!(0u64.index(), 0u64);
-        assert_eq!(9223372036854775807u64.index(), 9223372036854775807u64);
-        assert_eq!(9223372036854775808u64.index(), 0u64);
-        assert_eq!(18446744073709551615u64.index(), 9223372036854775807u64);
+    #[test]
+    fn test_is_leaf() {
+        assert_eq!(0.is_leaf(), false);
+        assert_eq!((usize::max_value() >> 1).is_leaf(), false);
+        assert_eq!(((usize::max_value() >> 1) + 1).is_leaf(), true);
+        assert_eq!(usize::max_value().is_leaf(), true);
     }
 }
